e22a0bf740ef4d5c6e1b0f87a6e779dd0c8b0eb6
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-16 04:00:42 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    true
+  else speed.between?(40, 60) 
+    false
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+  if speed > 60 || speed < 40
+    true
+  else speed.between?(40,60)
+    false
+  end
 end
 	
 
